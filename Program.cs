eb36d1f5947847ee3cdec67671b3efccd3d191de
UstbLww <35225878+UstbLww@users.noreply.github.com>
2018-01-08 22:28:15 +0800

    递归

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digui
+{
+    class Program
+    {
+        public void Cout(int inVal)
+        {
+            if (inVal == (0))
+                return;
+            Cout(inVal - 1);
+            Console.WriteLine("{0}", inVal);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Cout(3);
+        }
+    }
+}
